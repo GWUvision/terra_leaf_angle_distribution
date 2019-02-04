d5f843741908be610516000d157ed27de1c7afdf
ZongyangLi <zongyangli86@gmail.com>
2019-02-04 13:31:17 -0600

    Update ddpsc_leaf_angle.sh

--- ddpsc_leaf_angle.sh
+++ ddpsc_leaf_angle.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python /home/zli/pyWorkSpace/leafAngleDistribution/leafAngleDistribution.py -m all -p /home/zli/data/Terra/ua-mac/Level_1/scanner3DTop/ -j /home/zli/data/Terra/ua-mac/raw_data/scanner3DTop/ -o /home/zli/data/Terra/ua-mac/Level_2/leafAngleDistribution/ -v /home/zli/data/Terra/ua-mac/Level_2/leafAngleDistribution/result
+python /home/zli/pyWorkSpace/leafAngleDistribution/leafAngleDistribution.py -y 2018 -d 5 -f 5 -s 13 -e 13 -m date -p /media/zli/Elements/leaf_angle_debug/Level_1/scanner3DTop -j /media/zli/Elements/leaf_angle_debug/raw_data/scanner3DTop -o /media/zli/Elements/test/test_ua_mac/Level_2/leafAngleDistribution -v /media/zli/Elements/test/test_ua_mac/Level_2/leafAngleDistribution/result
